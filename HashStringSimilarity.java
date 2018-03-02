c61292580f2b41743eb7bd7fd0038c4fa007739b
swang <sfwang2@gmail.com>
2018-03-01 21:28:52 -0600

    update

--- HashStringSimilarity.java
+++ HashStringSimilarity.java
@@ -1,4 +1,17 @@
 
 public class HashStringSimilarity {
 
+	private String S;
+	private String T;
+	private int k;
+	
+	HashStringSimilarity(String s1, String s2, int sLength){
+		S = s1;
+		T = s2;
+		k = sLength;
+		
+	}
+	
+	
+	
 }
